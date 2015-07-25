c360a18b2dd10449350226f7cfddf51589fccdb5
Jared Roesch <roeschinc@gmail.com>
2015-07-24 17:08:50 -0700

    Add basic SnapshotTree implementation

--- src/librustc_data_structures/lib.rs
+++ src/librustc_data_structures/lib.rs
@@ -34,7 +34,11 @@
 extern crate core;
 #[macro_use] extern crate log;
 extern crate serialize as rustc_serialize; // used by deriving
+extern crate core;
 
+pub mod snapshot_vec;
+pub mod snapshot_tree;
+pub mod graph;
 pub mod bitvec;
 pub mod graph;
 pub mod ivar;
