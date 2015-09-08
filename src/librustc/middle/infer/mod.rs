62281a3d319bc099aef8f563b07352f119bee766
Jared Roesch <roeschinc@gmail.com>
2015-09-08 13:00:16 -0700

    Remove uncessary panic

--- src/librustc/middle/infer/mod.rs
+++ src/librustc/middle/infer/mod.rs
@@ -1936,7 +1936,7 @@ impl<'a, 'tcx> InferCtxt<'a, 'tcx> {
             Ok(predicate) => {
                 self.register_predicate_obligation(predicate)
             }
-            Err(ErrorReported) => { panic!() }
+            Err(ErrorReported) => {}
         }
     }
 
